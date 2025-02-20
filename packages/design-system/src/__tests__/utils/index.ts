02b6c7ee777947175f7ba6b186d3b91c15a41130
nerohalo <sassleismann@gmail.com>
2025-02-20 19:09:42 +0200

    test(design-system): add userEvent setup in test utils

--- packages/design-system/src/__tests__/utils/index.ts
+++ packages/design-system/src/__tests__/utils/index.ts
@@ -1,5 +1,10 @@
 import { configure } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
+// Applies strict mode setup to test for React-specific issues (like side effects and warnings).
 export function setupStrictMode() {
   configure({ reactStrictMode: true });
 }
+
+// Set up a userEvent instance for simulating user interactions. Remove artificial delay.
+export const user = userEvent.setup({ delay: null });
